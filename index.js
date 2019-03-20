034e3451f28fd4b48fa3274112725baf37ca42fe
Mikael Lönn <mikael@lonn.xyz>
2019-03-20 08:41:57 +0000

    things

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "website")));
+app.use(express.static(path.join(__dirname, "lonn.xyz")));
+app.use(express.static(path.join(__dirname, "old-projects/Ant")));
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "website", "index.html"));
+
+app.get("/ant", function(req, res) {
+  res.sendFile(path.join(__dirname, "old-projects/Ant", "ant.html"));
 });
-
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "lonn.xyz/build", "index.html"));
+});
 app.listen(9000);
